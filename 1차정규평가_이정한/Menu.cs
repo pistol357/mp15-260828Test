9b9b7f226ad78c9830a0fe92390213faf8925ae2
pistol357 <jeonghan615@gmail.com>
2026-08-28 10:24:19 +0900

    [Feat] 메뉴 클래스 생성자 작성

--- 1차정규평가_이정한/Menu.cs
+++ 1차정규평가_이정한/Menu.cs
@@ -4,4 +4,11 @@ public abstract class Menu
     protected string _name;
     protected int _price;
     protected MenuType _menuType;
+
+    public Menu(string name, int price, MenuType menuType)
+    {
+        _name = name;
+        _price = price;
+        _menuType = menuType;
+    }
 }
