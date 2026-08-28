5dd0805b134f222fc2ac41e9e464cae18025aeab
pistol357 <jeonghan615@gmail.com>
2026-08-28 16:35:43 +0900

    [Refactory] Dessert 클래스의 할인 기준 수량을 상수로 선언

--- 1차정규평가_이정한/Dessert.cs
+++ 1차정규평가_이정한/Dessert.cs
@@ -1,12 +1,13 @@
 ﻿
 public class Dessert : Menu
 {
+    const int DISCOUNT_ORDER_COUNT = 3;
     const float DISCOUNT_RATE = 0.9f;
     public Dessert(string name, int price) : base(name, price, MenuType.Dessert) { }
 
     public override int ReturnPrice()
     {
-        if (_orderCount >= 3)
+        if (_orderCount >= DISCOUNT_ORDER_COUNT)
         {
             return (int)(_price * _orderCount * DISCOUNT_RATE);
         }
@@ -15,6 +16,6 @@ public class Dessert : Menu
 
     public override void PrintMenuInfo()
     {
-        Console.WriteLine($"{Name} ({MenuType})  {Price}원 [3개 이상 구매 시 10% 할인]");
+        Console.WriteLine($"{Name} ({MenuType})  {Price}원 [{DISCOUNT_ORDER_COUNT}개 이상 구매 시 10% 할인]");
     }
 }
