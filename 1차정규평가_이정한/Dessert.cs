4c0bc0ee9990bec25e5ddb58f851763aa3956d11
pistol357 <jeonghan615@gmail.com>
2026-08-28 12:50:59 +0900

    [Style] 디저트 클래스 상수 const로 선언

--- 1차정규평가_이정한/Dessert.cs
+++ 1차정규평가_이정한/Dessert.cs
@@ -1,13 +1,14 @@
 ﻿
 public class Dessert : Menu
 {
+    const float DISCOUNT_RATE = 0.9f;
     public Dessert(string name, int price) : base(name, price, MenuType.Dessert) { }
 
     public override int ReturnPrice()
     {
         if(_orderCount >= 3)
         {
-            return (int)(base.ReturnPrice() * 0.9);
+            return (int)(base.ReturnPrice() * DISCOUNT_RATE);
         }
         return base.ReturnPrice();
     }
