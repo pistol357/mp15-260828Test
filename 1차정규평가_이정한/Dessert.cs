62bb2f9f952bea09a1e8b8668ef6ee4020d119ab
pistol357 <jeonghan615@gmail.com>
2026-08-28 16:45:24 +0900

    [Refactory] Dessert 클래스 할인율 관련 코드 수정

--- 1차정규평가_이정한/Dessert.cs
+++ 1차정규평가_이정한/Dessert.cs
@@ -2,20 +2,20 @@
 public class Dessert : Menu
 {
     const int DISCOUNT_ORDER_COUNT = 3;
-    const float DISCOUNT_RATE = 0.9f;
+    const float DISCOUNT_RATE = 0.1f;
     public Dessert(string name, int price) : base(name, price, MenuType.Dessert) { }
 
     public override int ReturnPrice()
     {
         if (_orderCount >= DISCOUNT_ORDER_COUNT)
         {
-            return (int)(_price * _orderCount * DISCOUNT_RATE);
+            return (int)(_price * _orderCount * (1- DISCOUNT_RATE));
         }
         return _price * _orderCount;
     }
 
     public override void PrintMenuInfo()
     {
-        Console.WriteLine($"{Name} ({MenuType})  {Price}원 [{DISCOUNT_ORDER_COUNT}개 이상 구매 시 10% 할인]");
+        Console.WriteLine($"{Name} ({MenuType})  {Price}원 [{DISCOUNT_ORDER_COUNT}개 이상 구매 시 {DISCOUNT_RATE * 100}% 할인]");
     }
 }
