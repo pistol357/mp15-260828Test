2f6525ba0036cc41a04b62d13c45cb38034e769d
pistol357 <jeonghan615@gmail.com>
2026-08-28 10:33:35 +0900

    [Feat] 손님의 행동을 묻는 메서드 선언과 호출

--- 1차정규평가_이정한/Program.cs
+++ 1차정규평가_이정한/Program.cs
@@ -4,8 +4,19 @@ using System;
 
 class Program
 {
+    const string CAFE_NAME = "정한 카페";
+
     static void Main(string[] args)
     {
+        while (true)
+        {
+            PrintCustomerAction();
+        }
+    }
 
+    public static void PrintCustomerAction()
+    {
+        Console.WriteLine("1. 장바구니에 메뉴 담기   2. 장바구니 전체 비우기   3. 결제   4. 영업 종료");
+        int action = ConsoleInput.ReadIntInRange("무엇을 원하시나요? : ", 1, 4);
     }
 }
