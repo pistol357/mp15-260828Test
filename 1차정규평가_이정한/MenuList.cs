c8c4ee0301ca72b321c6fe04daf7d4e35a101ed3
pistol357 <jeonghan615@gmail.com>
2026-08-28 11:03:34 +0900

    [Feat] 메뉴판 클래스 생성

---
+++ 1차정규평가_이정한/MenuList.cs
@@ -0,0 +1,42 @@
+﻿
+public class MenuList
+{
+    private Menu[] _menuList;
+    private int _count = 0;
+
+    public int Count
+    {
+        get
+        {
+            return _count;
+        }
+    }
+
+    public void SetMenuListSize(int size)
+    {
+        _menuList = new Menu[size];
+    }
+
+    public void AddMenu(Menu menu)
+    {
+        if(Count == _menuList.Length)
+        {
+            return;
+        }
+        _menuList[Count] = menu;
+    }
+
+    public Menu GetMenu(int index)
+    {
+        return _menuList[index];
+    }
+
+    public void PrintMenuList()
+    {
+        Console.WriteLine("[메뉴판]");
+        foreach(Menu menu in _menuList)
+        {
+            Console.WriteLine($"  {menu.ID}. {menu.Name} ({menu.MenuType})  {menu.Price}원");
+        }
+    }
+}
