b4832358e5e9eb04132b5d6e4ab34bf9091f7d9c
pistol357 <jeonghan615@gmail.com>
2026-08-28 12:00:09 +0900

    [Feat] 디저트 클래스 리턴프라이스 메서드 오버라이드 작성

--- 1차정규평가_이정한/Dessert.cs
+++ 1차정규평가_이정한/Dessert.cs
@@ -2,4 +2,13 @@
 public class Dessert : Menu
 {
     public Dessert(string name, int price) : base(name, price, MenuType.Dessert) { }
+
+    public override int ReturnPrice()
+    {
+        if(_orderCount >= 3)
+        {
+            return (int)(base.ReturnPrice() * 0.9);
+        }
+        return base.ReturnPrice();
+    }
 }
