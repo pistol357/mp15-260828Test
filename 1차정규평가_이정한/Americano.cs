31ec340b8841ee21384734e8cdaeac8a0d56ebd3
pistol357 <jeonghan615@gmail.com>
2026-08-28 11:08:30 +0900

    [Feat] 아메리카노 클래스 이름 변경

--- 1차정규평가_이정한/Americano.cs
+++
@@ -1,5 +0,0 @@
-﻿
-public class Americano : Menu
-{
-    public Americano(int id, string name, int price, MenuType menuType) : base(id, name, price, menuType) { }
-}
---
+++ 1차정규평가_이정한/Drink.cs
@@ -0,0 +1,5 @@
+﻿
+public class Drink : Menu
+{
+    public Drink(int id, string name, int price, MenuType menuType) : base(id, name, price, menuType) { }
+}
