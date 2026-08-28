14f5b5b6f8d8f93e30d24bd8e76bb06f5b597d19
pistol357 <jeonghan615@gmail.com>
2026-08-28 11:52:15 +0900

    [Feat] 카드 클래스 메뉴 담고 가격 출력하는 메서드 작성

--- 1차정규평가_이정한/Cart.cs
+++ 1차정규평가_이정한/Cart.cs
@@ -1,5 +1,28 @@
 ﻿
 public class Cart
 {
+    private List<Menu> _cart = new();
+    private int _totalCost = 0;
 
+    public void AddMenuToCart(Menu menu)
+    {
+        _cart.Add(menu);
+        menu.OrderCount++;
+    }
+
+    public void ClearCart()
+    {
+        _cart.Clear();
+    }
+
+    public void PrintCart()
+    {
+        Console.WriteLine("[장바구니]");
+        foreach(Menu menu in _cart)
+        {
+            Console.WriteLine($"  {menu.Name} X {menu.OrderCount} : {menu.ReturnPrice}원");
+        }
+        Console.WriteLine($"  합계 : {_totalCost}원");
+        Console.WriteLine("----------------------------------------");
+    }
 }
