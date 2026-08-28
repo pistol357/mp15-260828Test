8e608e1c22f37f2727016e617a667107f3b28cc3
pistol357 <jeonghan615@gmail.com>
2026-08-28 12:53:53 +0900

    [Feat] Cafe 클래스 _totalSales 멤버와 프로퍼티 작성

--- 1차정규평가_이정한/Cafe.cs
+++ 1차정규평가_이정한/Cafe.cs
@@ -1,4 +1,17 @@
 ﻿public class Cafe
 {
+    private int _totalSales;
 
+    public int TotalSales
+    {
+        get
+        {
+            return _totalSales;
+        }
+
+        set
+        {
+            _totalSales = value;
+        }
+    }
 }
