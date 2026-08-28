19a8fdf28e26c0887fc7bbd7d19f674a6cd376e2
pistol357 <jeonghan615@gmail.com>
2026-08-28 12:39:41 +0900

    [Feat] 프로그램 클래스 본문 작성

--- 1차정규평가_이정한/Program.cs
+++ 1차정규평가_이정한/Program.cs
@@ -9,10 +9,24 @@ class Program
 
     static void Main(string[] args)
     {
+        MenuList menuList = new MenuList();
+        menuList.SetMenuListSize(MENU_COUNT);
+        menuList.AddMenu(new Drink("아메리카노", 5000));
+        menuList.AddMenu(new Drink("카라멜 마끼아또", 6000));
+        menuList.AddMenu(new Drink("아이스티", 5000));
+        menuList.AddMenu(new Dessert("티라미수", 3800));
+        menuList.AddMenu(new Dessert("쿠키", 1000));
+
+        Cart cart = new Cart();
+
         while (true)
         {
+            Console.Clear();
             PrintCafeName();
-            PrintCustomerAction();
+            menuList.PrintMenuList();
+            cart.PrintCart();
+            PrintCustomerAction(menuList, cart);
+            ConsoleInput.Pause();
         }
     }
 
@@ -23,9 +37,38 @@ class Program
         Console.WriteLine("----------------------------------------");
     }
 
-    public static void PrintCustomerAction()
+    public static void PrintCustomerAction(MenuList menuList, Cart cart)
     {
         Console.WriteLine("1. 장바구니에 메뉴 담기   2. 장바구니 전체 비우기   3. 결제   4. 영업 종료");
         int action = ConsoleInput.ReadIntInRange("무엇을 원하시나요? : ", 1, 4);
+
+        if(action == (int)CustomerActionList.AddMenuToCart)
+        {
+            int menuToCart = ConsoleInput.ReadIntInRange("장바구니에 담을 메뉴를 선택해주세요. : ", 1, 5);
+            int menuToCartCount = ConsoleInput.ReadIntAtLeast("몇 개 담을까요? : ", 0);
+            cart.AddMenuToCart(menuList, menuToCart, menuToCartCount);
+        }
+        else if (action == (int)CustomerActionList.ClearCart)
+        {
+            cart.ClearCart();
+        }
+        else if (action == (int)CustomerActionList.Pay)
+        {
+            int pay = ConsoleInput.ReadIntAtLeast("받은 금액 : ", 0);
+
+            if(pay < cart.TotalCost)
+            {
+                Console.WriteLine("금액이 부족합니다.");
+            }
+            else
+            {
+                Console.WriteLine($"거스름돈 : {pay - cart.TotalCost}");
+                cart.ClearCart();
+            }
+        }
+        else if(action == (int)CustomerActionList.CloseCafe)
+        {
+
+        }
     }
 }
