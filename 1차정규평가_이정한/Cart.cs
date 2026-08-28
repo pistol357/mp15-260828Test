d5531de69850986c36606f3a282d9f4e3ce3d10b
pistol357 <jeonghan615@gmail.com>
2026-08-28 12:39:12 +0900

    [Feat] 장바구니 클래스 수정

--- 1차정규평가_이정한/Cart.cs
+++ 1차정규평가_이정한/Cart.cs
@@ -4,15 +4,26 @@ public class Cart
     private List<Menu> _cart = new();
     private int _totalCost = 0;
 
-    public void AddMenuToCart(Menu menu)
+    public int TotalCost
     {
+        get
+        {
+            return _totalCost;
+        }
+    }
+
+    public void AddMenuToCart(MenuList menuList, int menuNumber, int menuCount)
+    {
+        Menu menu = menuList.GetMenu(menuNumber - 1);
         _cart.Add(menu);
-        menu.OrderCount++;
+        menu.OrderCount += menuCount;
+        _totalCost += menu.ReturnPrice();
     }
 
     public void ClearCart()
     {
         _cart.Clear();
+        _totalCost = 0;
     }
 
     public void PrintCart()
@@ -20,7 +31,7 @@ public class Cart
         Console.WriteLine("[장바구니]");
         foreach(Menu menu in _cart)
         {
-            Console.WriteLine($"  {menu.Name} X {menu.OrderCount} : {menu.ReturnPrice}원");
+            Console.WriteLine($"  {menu.Name} X {menu.OrderCount} : {menu.ReturnPrice()}원");
         }
         Console.WriteLine($"  합계 : {_totalCost}원");
         Console.WriteLine("----------------------------------------");
